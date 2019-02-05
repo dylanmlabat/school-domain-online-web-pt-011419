b5844a08b8929b48780a046a8c177cf731968718
Dylan Michael Labat <dml252@live.com>
2019-02-05 23:32:15 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,18 +7,18 @@ class School
   end
   
   def add_student(name, grade)
-    if @roster.include?(grade) == false
-      @roster[grade] = []
+    if roster.include?(grade) == false
+      roster[grade] = []
     end
-    @roster[grade] << name
+    roster[grade] << name
   end
   
   def grade(grade)
-    @roster[grade]
+    roster[grade]
   end
   
   def sort
-    @roster.each do |key, value|
+    roster.each do |key, value|
       value.sort!
     end
   end
